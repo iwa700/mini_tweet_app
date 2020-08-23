abfa0f38b0650dfc8c3c7e65bb6986fdca6193e4
iwa700 <rizasaikou6@yahoo.co.jp>
2020-08-23 20:07:20 +0900

    ４間で

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'home#top'
+  get 'top', to: 'home#top'
   get 'about', to: 'home#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'posts/new', to: 'posts#new'
+  post 'posts/create', to: 'posts#create'
+  get 'posts/index', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show', as: :post
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  post 'posts/:id/update', to: 'posts#update', as: :update_post
+  post 'posts/:id/destroy', to: 'posts#destroy', as: :destroy_post
 end
